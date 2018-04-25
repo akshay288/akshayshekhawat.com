03193ca17b447012082a6cc326bc4bf40b6c458f
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2018-04-25 11:43:08 -0500

    syntax highlighting + links in new tabs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,20 @@ var gulp =   require('gulp');
 var pug =    require("gulp-pug");
 var uglify = require("gulp-uglify");
 
+var transformMarkdown = function(text) {
+	var md = require('markdown-it')();
+	var defaultRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+		return self.renderToken(tokens, idx, options);
+	};
+	
+	md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+		tokens[idx].attrPush(['target', '_blank']);
+		return defaultRender(tokens, idx, options, env, self);
+	};
+
+	return md.render(text)
+};
+
 gulp.task("css", function() {
 	return gulp.src("src/css/*.css")
 		.pipe(gulp.dest("dist"));
@@ -15,10 +29,7 @@ gulp.task("pug-index", function() {
 
 gulp.task("pug-blog", function() {
 	pug_filters = {
-		'markdown': function (text) {
-			var md = require('markdown-it')().use(require('markdown-it-sub'));
-			return md.render(text)
-		}
+		'markdown': transformMarkdown
 	}
 	return gulp.src("src/pug/blog/*.pug")
 		.pipe(pug({'filters': pug_filters}))
@@ -36,9 +47,14 @@ gulp.task("files", function() {
 		.pipe(gulp.dest("dist/files"));
 });
 
+gulp.task("highlightjs", function() {
+	return gulp.src(["node_modules/highlightjs/styles/solarized-dark.css", "node_modules/highlightjs/highlight.pack.min.js"])
+		.pipe(gulp.dest("dist"));
+});
+
 gulp.task("keybase", function() {
 	return gulp.src("src/keybase.txt")
 		.pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", ["files", "js", "css", "pug-index", "pug-blog", "keybase"]);
+gulp.task("default", ["files", "js", "css", "pug-index", "pug-blog", "keybase", "highlightjs"]);
